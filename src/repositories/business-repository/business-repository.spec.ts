f50c5da20c015c7f671151ecd27b13197724f5a2
Ben Tlusty <tbtlusty@gmail.com>
2022-07-16 22:35:30 +0300

    Fixing tests in CI

--- src/repositories/business-repository/business-repository.spec.ts
+++ src/repositories/business-repository/business-repository.spec.ts
@@ -1,11 +1,12 @@
 import businessRepository from "./business-repository";
 import * as fs from "fs";
+import path from "path";
 
 describe("Business Repository", () => {
-  const path = "./tmp/tmp-businesses.json";
+  const jsonPath = path.join(__dirname, "../../../tmp/businesses.json");
 
   afterEach(async () => {
-    await fs.promises.unlink(path);
+    await fs.promises.unlink(jsonPath);
   });
 
   it("should return an Original Business Name to normalize Business Name", async () => {
@@ -14,19 +15,19 @@ describe("Business Repository", () => {
       [originalBusinessName]: "Super Yoda",
     };
     await fs.promises.writeFile(
-      path,
+      jsonPath,
       JSON.stringify(originalBusinessNameToNormalized),
       "utf-8"
     );
-    const result = await businessRepository.getBusinesses(path);
+    const result = await businessRepository.getBusinesses(jsonPath);
 
     expect(result).toStrictEqual({ [originalBusinessName]: "Super Yoda" });
   });
 
   it("should save a new Business Name", async () => {
-    await businessRepository.setBusinesses(path, { business: "10" });
+    await businessRepository.setBusinesses(jsonPath, { business: "10" });
 
-    const expectedResult = await fs.promises.readFile(path, "utf-8");
+    const expectedResult = await fs.promises.readFile(jsonPath, "utf-8");
     expect(JSON.parse(expectedResult)).toStrictEqual({ business: "10" });
   });
 });
